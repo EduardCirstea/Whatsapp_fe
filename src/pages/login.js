f6ae4a19e46da1fed7c68701647dfc5d6c626edf
Eduard <eduard_e28@yahoo.com>
2024-07-17 18:40:09 +0300

    working on the frontend authentification

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,13 @@
-import React from "react";
+import LoginForm from "../components/auth/LoginForm";
 
-function login() {
-  return <div>login</div>;
+export default function Login() {
+  return (
+    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
+      {/*Container */}
+      <div className="flex w-[1600px] mx-auto h-full">
+        {/*Login form*/}
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
-
-export default login;
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,13 @@
-import React from "react";
+import RegisterForm from "../components/auth/RegisterForm";
 
-function register() {
-  return <div>register</div>;
+export default function Register() {
+  return (
+    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden">
+      {/*Container */}
+      <div className="flex w-[1600px] mx-auto h-full">
+        {/*Register form */}
+        <RegisterForm />
+      </div>
+    </div>
+  );
 }
-
-export default register;
