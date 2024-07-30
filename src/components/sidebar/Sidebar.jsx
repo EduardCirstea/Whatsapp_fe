01b53948aefe555359edbc85a318323854817799
Eduard <eduard_e28@yahoo.com>
2024-07-31 00:51:34 +0300

    Created the search components

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
 import { SidebarHeader } from "./header";
 import { Notifications } from "./notifications";
-import { Search } from "./search";
+import { Search, SearchResults } from "./search";
 import { Conversations } from "./conversations";
 
 export default function Sidebar() {
   const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="w-[40%] h-full select-none">
       <SidebarHeader />
       <Notifications />
-      <Search searchLength={searchResults.length} />
-      <Conversations />
+      <Search
+        searchLength={searchResults.length}
+        setSearchResults={setSearchResults}
+      />
+      {searchResults.length > 0 ? (
+        <>
+          <SearchResults searchResults={searchResults} />
+        </>
+      ) : (
+        <>
+          <Conversations />
+        </>
+      )}
     </div>
   );
 }
