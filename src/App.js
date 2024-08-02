7eb3688f7adeadeb6c07c8bf5e1f83b6bc19ce4c
Eduard <eduard_e28@yahoo.com>
2024-08-02 16:22:10 +0300

    installed socket io

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { io } from "socket.io-client";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 //Pages
 import Home from "./pages/home";
 import Register from "./pages/register";
 import Login from "./pages/login";
-import { useSelector } from "react-redux";
+//socket io
+const socket = io(process.env.REACT_APP_API_ENDPOINT.split("/api/v1")[0]);
 
 function App() {
-  const { user } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => ({ ...state }));
   const { token } = user;
+
   return (
     <div className="dark">
       <Router>
